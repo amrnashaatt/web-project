0a18d4ac2981e4ebf66f64aa4d9423beccd2caa3
seifbedair <seifbedair20@gmail.com>
2024-01-02 10:16:29 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -11,21 +11,20 @@ if ($conn->connect_error) {
 }
 
 // Get form data
-$email = $_POST['email'];
+$email = mysqli_real_escape_string($conn, $_POST['email']);
 
 // Perform SQL insertion
 $sql = "INSERT INTO volunteerss (email) VALUES ('$email')";
 
 if ($conn->query($sql) === TRUE) {
-    // If the insertion is successful, you can redirect to a thank you page or display a success message.
-    echo "Thank you for registering! We appreciate your support. We will reach you as soon as possible.";
+    // If the insertion is successful, redirect to a thank you page or display a success message.
+    header("Location: thank_you_page.php");
+    exit();
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    // Log the error and provide feedback to the user.
+    error_log("Error: " . $sql . "\n" . $conn->error, 0);
+    echo "Error processing your registration. Please try again later.";
 }
-
-
-
-
 
 // Close the connection
 $conn->close();
