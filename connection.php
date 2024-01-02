0fc929944df84d2fa2230560221ee2db662b8b1d
seifbedair <seifbedair20@gmail.com>
2024-01-02 03:41:54 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = mysqli_real_escape_string($conn, $_POST['email']);
 
-    $sql = "INSERT INTO volunteers (email) VALUES ('$email')";
+    $sql = "INSERT INTO volunteerss (email) VALUES ('$email')";
 
     if (mysqli_query($conn, $sql)) {
         echo "Thank you for registering! We appreciate your support. We will reach out to you as soon as possible.";
